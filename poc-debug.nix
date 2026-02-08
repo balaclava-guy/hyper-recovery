dccf9ec3716d1cc3ddfc7b90ef1095c24d9f858e
balaclava-guy <blclvgy@proton.me>
2026-02-08 14:58:12 +0000

    fix: use util-linuxMinimal in initrd

--- poc-debug.nix
+++ poc-debug.nix
@@ -62,7 +62,7 @@
 
   boot.initrd.systemd = {
     enable = true;
-    initrdBin = [ pkgs.coreutils pkgs.util-linux-minimal ];
+    initrdBin = [ pkgs.coreutils pkgs.util-linuxMinimal ];
     services.initrd-logdump = {
       description = "Dump initrd logs to Ventoy";
       wantedBy = [ "initrd.target" ];
