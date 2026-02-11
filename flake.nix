6c2ae08e6a695ad9d4e06f22770bf7439c1d6750
balaclava-guy <blclvgy@proton.me>
2026-02-11 02:13:29 +0000

    chore: picking up llm toys

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Snosu Hyper Recovery Environment";
+  description = "Super Nixos Utilities: Hyper Recovery";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
